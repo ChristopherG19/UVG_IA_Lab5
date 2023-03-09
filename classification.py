09cc4dc3a39a0430ba5069e7ed340d69d7b4cc6e
ChrisG19 <christopherg2808@gmail.com>
2023-03-09 10:03:06 -0600

    SVM library implementada

--- classification.py
+++ classification.py
@@ -12,8 +12,6 @@ df = pd.read_csv('./dataset_phishing.csv')
 data_encoded = pd.get_dummies(df, columns=['status'], prefix='', prefix_sep='')
 df = data_encoded
 
-df.info()
-
 df["legitimate"] = df["legitimate"].astype('int64')
 df["phishing"] = df["phishing"].astype('int64')
 to_categoric = ["url"]
@@ -22,10 +20,6 @@ df = clean(df, method = 'dtypes', columns = to_categoric, dtype='category')
 
 # eliminar la columna legitamate para no tener información repetida
 df = df.drop('legitimate', axis = 1)
-
-df.info()
-
-df.describe()
 
 # Obtención de valores
 X = df.iloc[:, 2:-1].values
@@ -40,18 +34,6 @@ print(y)
 from sklearn.model_selection import train_test_split
 X_entreno, X_prueba, y_entreno, y_prueba = train_test_split(X, y, test_size = 0.2, random_state = 100)
 
-# balanceo de datos
-
-
-
-
-
-
-
-
-
-
-
 # Task 1.2 Support Vector Machines
 from SVM import *
 clf = SVM(n_iters=1000)
@@ -62,4 +44,27 @@ def accuracy(y_true, y_pred):
     accuracy = np.sum(y_true==y_pred) / len(y_true)
     return accuracy
 
+# Task 1.2 impresión resultados
+from sklearn.svm import SVC
+from sklearn.metrics import confusion_matrix, accuracy_score, classification_report
+
+# Sin librerías
 print("SVM Accuracy: ", accuracy(y_prueba, predictions))
+mat_conf = confusion_matrix(y_prueba, predictions)
+print("\nMatriz de confusión: ")
+print(mat_conf)
+print("\nReporte de clasificación")
+print(classification_report(y_prueba, predictions))
+print()
+
+# Con librerías
+clasificador = SVC(kernel = 'rbf', random_state = 2)
+clasificador.fit(X_entreno, y_entreno)
+y_pred = clasificador.predict(X_prueba)
+np.concatenate((y_pred.reshape(len(y_pred),1), y_prueba.reshape(len(y_prueba),1)),1)
+mat_conf = confusion_matrix(y_prueba, y_pred)
+print("\nMatriz de confusión: ")
+print(mat_conf)
+print("\nReporte de clasificación")
+print(classification_report(y_prueba, y_pred))
+print()
