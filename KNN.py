8cff20133703f5a6cb85b5e26f0882fe5e5f5342
Isabel Solano <misolanob@gmail.com>
2023-03-09 00:46:19 -0600

    KNN init

---
+++ KNN.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+class KNN:
+    def __init__(self):
+        pass
+
+    def _eucledian(p1,p2):
+        dist = np.sqrt(np.sum((p1-p2)**2))
+        return dist
+
+    def fit(self, X, y):
+        pass
+
+    def predict():
+        pass
+
+    
